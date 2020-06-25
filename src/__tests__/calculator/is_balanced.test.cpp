1556395cd6a7ae2535cd9500e7da99b5da88b89c
Yudi Yamane <yudiyamane@gmail.com>
2020-06-25 20:25:49 -0300

    arrumei string do caminho dos arquivos de teste

--- src/__tests__/calculator/is_balanced.test.cpp
+++ src/__tests__/calculator/is_balanced.test.cpp
@@ -31,14 +31,13 @@ TEST_CASE("tests on is_balanced", "[is_balanced]") {
     const int NUMBER_OF_TESTS = 58;
     const int EXPRESSION_LEN_MAX = 100500; // Cem mil e quinhentos caracteres
                                            // é o maior número nos casos de teste.
-
     char test_filename[FILENAME_MAX];   // Vem de stdio.h
     char answer_filename[FILENAME_MAX]; // Vem de stdio.h
     char expression[EXPRESSION_LEN_MAX];
 
     // Se os executáveis mudarem de diretório, tem que mudar essas linhas?.
-    const char filename1[] = "../tests/calculator/check_brackets_in_code/%02d";
-    const char filename2[] = "../tests/calculator/check_brackets_in_code/%02d.a";
+    const char filename1[] = "../src/__tests__/calculator/check_brackets_in_code/%02d";
+    const char filename2[] = "../src/__tests__/calculator/check_brackets_in_code/%02d.a";
 
     string test_line, answer_line;
 
@@ -53,21 +52,23 @@ TEST_CASE("tests on is_balanced", "[is_balanced]") {
             test_file.open(test_filename);
             answer_file.open(answer_filename);
 
-            if(test_file.is_open() && answer_file.is_open()) {
-                getline(test_file, test_line);
-                getline(answer_file, answer_line);
-
-                snprintf(expression, EXPRESSION_LEN_MAX, "%s", test_line.c_str());
-
-                if (answer_line == "Success\n")
-                    // Quase que eu começo a perder a cabeça por causa do barra n.
-                    REQUIRE(is_balanced(expression) == 0);
-                else
-                    REQUIRE(is_balanced(expression) == 0);
-
-                test_file.close();
-                answer_file.close();
+            if (!test_file.is_open() || !answer_file.is_open()) {
+                throw std::invalid_argument("Error opening file");
             }
+
+            getline(test_file, test_line);
+            getline(answer_file, answer_line);
+
+            snprintf(expression, EXPRESSION_LEN_MAX, "%s", test_line.c_str());
+
+            if (answer_line == "Success\n")
+                // Quase que eu começo a perder a cabeça por causa do barra n.
+                REQUIRE(is_balanced(expression) == 0);
+            else
+                REQUIRE(is_balanced(expression) == 0);
+
+            test_file.close();
+            answer_file.close();
         }
     }
 }
