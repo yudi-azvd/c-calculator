56b8aae0270bde52784db97e271965fa58781cb3
Yudi Yamane <yudiyamane@gmail.com>
2020-12-17 10:32:44 -0300

    doctest worked on linux, but it didn\'t on Windows 10 and I did the same thing

---
+++ src/__tests__/doctest/test_doctest.test.cpp
@@ -0,0 +1,47 @@
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include "../../lib/doctest/doctest.h"
+
+using std::string;
+
+#ifndef _TEST_CALCULATOR
+#define _TEST_CALCULATOR
+extern "C" {
+#endif
+
+#include <stdio.h>
+#include <string.h>
+#include "../../core/calculator.h"
+
+#ifdef _TEST_CALCULATOR
+}
+#endif
+
+
+int factorial(int number) { return number <= 1 ? number : factorial(number - 1) * number; }
+
+TEST_CASE("testing the factorial function") {
+  CHECK(factorial(1) == 1);
+  CHECK(factorial(2) == 2);
+  CHECK(factorial(3) == 6);
+  CHECK(factorial(10) == 3628800);
+}
+
+TEST_CASE("list") {
+  char* data;
+  char* result;
+  t_list* list;
+
+  char expression[] = "3*(8+4)/2";
+
+  list = expression_to_list(expression);
+
+  std::cout << "this is working" << std::endl;
+
+  evaluate(list, &result);
+  CHECK(string(result) == "18.000000");
+
+  clear(list); // liberar os nós e os data's
+  free(list);
+
+  free(result);
+}
